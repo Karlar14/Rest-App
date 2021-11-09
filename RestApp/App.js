fd3ab46fcac9367150d1d44940eebb67cc3ce367
Camilla <camille.ragland@gmail.com>
2021-11-08 19:41:28 -0500

    setting up hello world

--- RestApp/App.js
+++ RestApp/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("App Executed");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
