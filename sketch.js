164d5de7432b2edb710cceba842f67c0bd9670cf
Camilla <camille.ragland@gmail.com>
2021-12-02 18:15:57 -0500

    p5

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ const { Part } = require("c:/users/12403/.vscode/extensions/samplavigne.p5-vscod
 var song
 var fft
 var img
-// var particles = []
+
+let rain = []
+
 
 function preload(){
   song = loadSound('Clair de Lune With Rain.mp3')
@@ -11,15 +13,17 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight)
+  background(0)
   angleMode(DEGREES)
   imageMode(CENTER)
   img.filter(BLUR, 2)
   fft = new p5.FFT()
+  rain.push(new Drop())
 }
 
 function draw() {
-  background(0)
+  
   stroke(255)
   strokeWeight(3)
   noFill()
@@ -31,29 +35,30 @@ function draw() {
   fft.analyze()
   amp = fft.getEnergy(20,200)
 
-  var wave = fft.waveform()
+//   var wave = fft.waveform()
 
-  for (var t = -1; t <= 1; t += 2){
-    beginShape()
-  for(var i = 0; i <= 180; i+= 0.3){
-    var index = floor (map(i, 0, width, 0 , wave.length-1))
+//   for (var t = -1; t <= 1; t += 2){
+//     beginShape()
+//   for(var i = 0; i <= 180; i+= 0.3){
+//     var index = floor (map(i, 0, width, 0 , wave.length-1))
     
-    var r = map(wave[index], -1, 1, 150, 350)
+//     var r = map(wave[index], -1, 1, 150, 350)
     
-    var x = r*sin(i) * t
-    var y = r*cos(i)
-    vertex(x,y)
-  }
-  endShape()
-}
-
-// var p = new Particle()
-// particles.push(p)
-
-// for(var i=0; i<particles.length; i++){
-//   particles[i].show()
+//     var x = r*sin(i) * t
+//     var y = r*cos(i)
+//     vertex(x,y)
+//   }
+//   endShape()
 // }
 
+var d = new Drop
+
+
+for (var i=rain.length-1; i >= 0 ; i-- )
+{
+ d.fall()
+d.show()
+}
 }
 
 function mouseClicked(){
@@ -66,14 +71,20 @@ function mouseClicked(){
   }
 }
 
-// class Particle{
-//   constructor(){
-//     this.pos = p5.Vector.random2D().mult(250)
-//   }
+class Drop{
+  constructor(){
+    this.pos = createVector()
+    this.vel = createVector()
+    this.acc = createVector()
+  }
 
-//   show(){
-//     noStroke()
-//     fill(random)
-//     ellipse(this.pos.x, this.pos.y, 4)
-//   }
-// }
+
+  render()
+  {
+    stroke(255)
+    strokeWeight(4)
+    line(this.pos.x,this.pos.y,this.pos.x+3,this.pos.x+3)
+  }
+
+}
+
