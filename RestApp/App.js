469a95c1d63abb597be60921aa72e89ca10a16ef
shanice s <shanices@shanices-MBP.lan>
2021-12-06 19:19:47 -0500

    added functions for each screen

--- RestApp/App.js
+++ RestApp/App.js
@@ -4,14 +4,73 @@ import { Video, Audio, AVPlaybackStatus } from 'expo-av';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
+function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.homeScreen}>
+      <Text style={styles.baseText}>Do you need a moment?</Text>
+      <Text style={styles.baseText}>Come on in</Text>
+      <Text style={styles.baseText}>This is...</Text>
+      <Text style={styles.titleText}>Rest</Text>
+      <Button
+        title="Continue"
+        onPress={() => navigation.navigate('Questionnaire')} />
+    </SafeAreaView>
+  );
+}
 
-export default function App() {
+function Questionnaire() {
+  return (
+    <SafeAreaView style={styles.Questionnaire}>
+      <Text>Questionnaire</Text>
+    </SafeAreaView>
+  );
+}
+
+function LandingPage() {
+  return (
+    <SafeAreaView style={styles.landingPage}>
+      <Text>Landing Page</Text>
+    </SafeAreaView>
+  );
+}
+
+function Search() {
+  return (
+    <SafeAreaView style={styles.search}>
+      <Text>Search Page</Text>
+    </SafeAreaView>
+  );
+}
+
+function Notifications() {
+  return (
+    <SafeAreaView style={styles.notifications}>
+      <Text>Notifications</Text>
+    </SafeAreaView>
+  );
+}
+
+function Settings() {
+  return (
+    <SafeAreaView style={styles.Settings}>
+      <Text>Settings</Text>
+    </SafeAreaView>
+  );
+}
+
+function audioVisualizer() {
+  return (
+    <View>
+      <Text>Audio Visualizer</Text>
+    </View>
+  );
+}
+
+function addVideo() {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
+    <View>
         <Video
           ref={video}
           style={styles.video}
@@ -22,28 +81,39 @@ export default function App() {
           resizeMode="contain"
           isLooping
           onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
         />
-        <View style={styles.buttons}>
-          <Button
-            title={status.isPlaying ? 'Pause' : 'Play'}
-            onPress={() =>
-              status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-            }
-          />
-        </View>
-        
-          <Text style={styles.baseText}>Do you need a moment?</Text>
-          <Text style={styles.baseText}>Come on in</Text>
-          <Text style={styles.baseText}>This is...</Text>
-          <Text style={styles.titleText}>Rest</Text>
-          <Button title="Continue" />
-      </SafeAreaView>
+      </View>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Welcome" component={HomeScreen} />
+        <Stack.Screen name="Questionnaire" component={Questionnaire} />
+        <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="Search" component={Search} />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen name="AudioViz" component={audioVisualizer} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  homeScreen: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: "center",
@@ -55,5 +125,11 @@ const styles = StyleSheet.create({
   },
   titleText: {
     fontSize: 20,
-  }
+  },
+  Questionnaire: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
